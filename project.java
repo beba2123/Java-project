ec482d96ca863a07ac4cfd81f2ce505ff19a5629
beba2123 <antenehbeba@gmail.com>
2023-05-21 14:30:37 +0300

    updated project commit

--- project.java
+++ project.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 class product{ 
     private int warranty;
     private String name;
@@ -59,29 +60,62 @@ class serviceAddingProucts{
     public void addProduct(product obj){
         products.add(obj); 
     }
-    public List<product> getProducts(){
+    public List<product> getAllProducts(){
         return products;
     }
+    public product getSingleProduct(String name){  
+          for(product p : products){
+            if(p.getname().equals(name)){
+                return p;
+            // }else if(p.gettype().equals(type)){
+            //     return p;
+            // }
+          }
+        }
+        return null;
+    }
+    public product getplace(String place){
+        for(product p : products){
+            if(p.getPlace().equals(place)){
+                return p;
+            }
+        }
+        return null;
+    }
+    public List<product> getProductsWithTxt(String text){
+        String str = text.toLowerCase();
+        List<product> prods = new ArrayList<>();
+            for(product p : products){
+                String name = p.getPlace().toLowerCase();
+                String type = p.getname().toLowerCase();
+                String place = p.getPlace().toLowerCase();
+                if(name.contains(str) || type.contains(str) || place.contains(str)){
+                    prods.add(p);
+                }
+            }
+        return prods;
+    }
 }
-
-
-
-
-
 
 public class project{
     public static void main(String[] args) {
         serviceAddingProucts obj2 = new serviceAddingProucts();
         // product obj = 
-        obj2.addProduct(new product("laptop" , "Asus-vivo", "Adiss Ababa",  2));
-        obj2.addProduct(new product("laptop-1" , "Lenovo", "Adiss Ababa",  2));
-        obj2.addProduct(new product("laptop-2" , "Hp-Pavilion", "Adiss Ababa",  2));
-        obj2.addProduct(new product("laptop-3" , "Dell", "Adiss Ababa",  2));
+        obj2.addProduct(new product("laptop" , "Adiss Abeba", "Asus-Nitro",  2));
+        obj2.addProduct(new product("laptop-1" , "bishoftu", "Lenovo-yoga",  2));
+        obj2.addProduct(new product("laptop-2" , "Adama", "Hp-Pavillion",  2));
+        obj2.addProduct(new product("laptop-3" , "Dukem", "Dell-optima",  2));
 
-        List<product> products = obj2.getProducts(); //give me all  the product inside the list..
-        for(product Li : products){
-            System.out.println(Li);
+        // List<product> products = obj2.getAllProducts(); //give me all  the product inside the list..
+        // for(product Li : products){
+        //     System.out.println(Li);
+        // }
+    //     product p = obj2.getSingleProduct("laptop");
+    //     System.out.println(p);
+    //    System.out.println(obj2.getplace("bishoftu"));
+        List<product> prods = obj2.getProductsWithTxt("ad");
+        for(product p1 : prods){
+        System.out.println(p1);
         }
-       
     }
 }
